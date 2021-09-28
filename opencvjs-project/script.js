2c57fe26797d5aef3692ce3dac7906f071ef0d47
Quinn Spraut <quinn.a.spraut.22@dartmouth.edu>
2021-09-28 15:59:26 -0400

    image download complete

--- opencvjs-project/script.js
+++ opencvjs-project/script.js
@@ -17,33 +17,48 @@ imgElement.onload = function() {
 };
 
 document.getElementById('circlesButton').onclick = function() {
+  // disable page
   this.disabled = true;
   document.body.classList.add('loading');
 
+  // circle detection code
   let srcMat = cv.imread('imageCanvas');
   let displayMat = srcMat.clone();
   let circlesMat = new cv.Mat();
 
-  cv.cvtColor(srcMat, srcMat, cv.COLOR_RGBA2GRAY);
+  cv.cvtColor(srcMat, srcMat, cv.COLOR_RGBA2GRAY); // convert to grayscale
+  cv.HoughCircles(srcMat, circlesMat, cv.HOUGH_GRADIENT, 1, 45, 75, 40, 0, 0); // detect circles
 
-  cv.HoughCircles(srcMat, circlesMat, cv.HOUGH_GRADIENT, 1, 45, 75, 40, 0, 0);
-
+  // draw circles
   for (let i = 0; i < circlesMat.cols; ++i) {
     let x = circlesMat.data32F[i * 3];
     let y = circlesMat.data32F[i * 3 + 1];
     let radius = circlesMat.data32F[i * 3 + 2];
     let center = new cv.Point(x, y);
-
-    // draw circles
     cv.circle(displayMat, center, radius, [0, 0, 0, 255], 3);
   }
 
-  cv.imshow('imageCanvas', displayMat);
+  cv.imshow('imageCanvas', displayMat); // display the circles
 
+  // cleanup
   srcMat.delete();
   displayMat.delete();
   circlesMat.delete();
 
+  // reenable page
   this.disabled = false;
   document.body.classList.remove('loading');
+};
+
+let debounceDownload = false;
+document.getElementById('downloadButton').onclick = function() {
+  if (!debounceDownload) {
+    debounceDownload = true;
+    setTimeout(() => {
+      debounceDownload = false;
+    }, 100);
+
+    this.href = document.getElementById('imageCanvas').toDataURL();
+    this.download = 'image.png';
+  }
 };
